ef8357e5acde9a1cd3243b6b63a61d1a7de529f9
ratulbd <ratulbracu@gmail.com>
2025-11-10 16:41:18 +0600

    More sensetive changes V88

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -85,7 +85,7 @@ const HEADER_MAPPING = {
     // Bank
     bankAccount: 'bankaccountnumber',
     // Status & History
-    status: 'status', salaryHeld: 'salaryheld', holdTimestamp: 'holdtimestamp',
+    status: 'Status', salaryHeld: 'salaryheld', holdTimestamp: 'holdtimestamp',
     separationDate: 'separationdate', remarks: 'remarks',
     lastTransferDate: 'lasttransferdate', lastSubcenter: 'lastsubcenter',
     lastTransferReason: 'lasttransferreason',
